25aebf755ec7efa38c3a195092f142a7a84c869f
kevinw1222 <homingkevin.wong@edu.sait.ca>
2023-10-18 13:43:16 -0600

    Update Sort.java
    changes on Insertion / Selection

--- src/complexityandsorting/drivers/sort/Sort.java
+++ src/complexityandsorting/drivers/sort/Sort.java
@@ -16,14 +16,34 @@ public class Sort {
         
     }
     
-    public static void Insertion(Shape[] shapes)
+    public static void Insertion(Comparable[] data)
     {
-        
+        int n =data.length;
+        for(int i=1;i<n;i++){
+            Comparable key =shapes[i];
+            int j =i-1;
+            while(j>=0&&data[j].compareTo(key)>0){
+                data[j+1]=data[j];
+                j=j-1;
+            }
+            data[j+1]=key;
+        }
     }
     
-    public static void Selection(Shape[] shapes)
+    public static void Selection(Comparable[] data)
     {
-        
+        int n =data.length;
+        for(int i =0;i<n-1;i++){
+            int minIndex =i;
+            for(int j =j+1;j<n;j++){
+                if(data[j].compareTo(data[minIndex])<0){
+                    minIndex=j;
+                }
+            }
+            Comparable temp=data[minIndex];
+            data[minIndex]=data[i];
+            data[i]=temp;
+        }
     }
     
     public static void Merge(Shape[] shapes)
